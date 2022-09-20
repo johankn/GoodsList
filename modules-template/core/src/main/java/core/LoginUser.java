3f71311f45ab30dbc3d6015131855fb0bb927db5
Elias Lysø Sommerseth <elias.sommerseth@gamil.com>
2022-09-20 11:30:20 +0200

    Implement user login
    Made LoginUser.java witch is a class that represents a user that is trying to login. Aslo made the LoginController.java class, but its is not 100% done yet (it does not have the logic for writing to file). The userInterface for the loginpage is finished and named Login.fxml.

---
+++ modules-template/core/src/main/java/core/LoginUser.java
@@ -0,0 +1,21 @@
+package core;
+
+public class LoginUser {
+    
+    private String username;
+    private String password;
+
+    public LoginUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+}
