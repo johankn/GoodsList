9ed4e4af6e280f33d5020c6f1246285d8b3e3e33
johakn <johakn@stud.ntnu.no>
2022-09-27 11:44:18 +0200

    not finished yet, but started on Ad.java and Product.java

---
+++ GoodsList/core/src/main/java/core/Ad.java
@@ -0,0 +1,14 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Ad {
+    private Product productName;
+    private User owner;
+    private Date date;
+    private String location;
+    private ArrayList<Integer> offers;
+    private String description;
+
+}
---
+++ GoodsList/core/src/main/java/core/Product.java
@@ -0,0 +1,7 @@
+package core;
+
+public class Product {
+    private int price;
+    private String condition;
+    private String productTitle;
+}
