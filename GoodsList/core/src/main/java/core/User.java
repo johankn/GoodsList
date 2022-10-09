c88654a0dbe6898c8df08d40c8e84652f92abf40
johakn <johakn@stud.ntnu.no>
2022-10-09 16:27:50 +0200

    added User class

---
+++ GoodsList/core/src/main/java/core/User.java
@@ -0,0 +1,11 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    List<Ad> activeAds = new ArrayList<Ad>();
+    List<Ad> inactiveAds = new ArrayList<Ad>();
+    String name;
+    
+}
