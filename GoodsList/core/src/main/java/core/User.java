022828f97f8960a0474683f44d53eaf9137ae03d
emilkl <emilkl@stud.ntnu.nogit pushgit push -fmvn test>
2022-10-11 11:11:29 +0200

    Correct incapsulation
    Updated the attributes of the user class to hace correct incapsulation

--- GoodsList/core/src/main/java/core/User.java
+++ GoodsList/core/src/main/java/core/User.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    List<Ad> activeAds = new ArrayList<Ad>();
-    List<Ad> inactiveAds = new ArrayList<Ad>();
-    String name;
+    private List<Ad> activeAds = new ArrayList<Ad>();
+    private List<Ad> inactiveAds = new ArrayList<Ad>();
+    private String name;
 
     void addAdToList(Ad ad) {
         activeAds.add(ad);
