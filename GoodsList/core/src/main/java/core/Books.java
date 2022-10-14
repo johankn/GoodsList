b798b3f7ac2b3aa8f76493361b9b362d17ac86f5
mavalhov <mavalhov@stud.ntnu.no>
2022-10-14 12:04:16 +0200

    Added Jacksjon annotation

--- GoodsList/core/src/main/java/core/Books.java
+++ GoodsList/core/src/main/java/core/Books.java
@@ -13,7 +13,14 @@ public class Books extends Product {
     private String genre;
     private int releaseYear;
 
-    public Books(int price, String condition, String author, String genre, int releaseYear, int pages) {
+    @JsonCreator
+    public Books(
+    @JsonProperty(value = "price") int price,
+    @JsonProperty(value = "condition") String condition,
+    @JsonProperty(value = "author") String author,
+    @JsonProperty(value = "genre") String genre,
+    @JsonProperty(value = "releaseYear") int releaseYear,
+    @JsonProperty(value = "pages") int pages) {
         super(price, condition);
         this.pages = pages;
         this.author = author;
