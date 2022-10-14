1d747b4fa17d9e716a1b1132ec9cf5c1440fb8a9
Elias Lysø Sommerseth <elias.sommerseth@gamil.com>
2022-10-14 19:49:54 +0200

    Improved LoginValidatorTest.java and updated LoginValidator.java
    LoginValidator.java is now up to date with the new implementation of how to erite a user to file. LoginValidatorTest.java is aslo up to date with this. I aslo improved the testing a bit by using User.java-objects in the test insted og Stringfields.

--- GoodsList/core/src/test/java/core/LoginValidatorTest.java
+++ GoodsList/core/src/test/java/core/LoginValidatorTest.java
@@ -1,5 +1,9 @@
 package core;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,57 +12,70 @@ public class LoginValidatorTest {
     //private static final String filename = "..//core/src/main/java/json/dataObjects.json";
     private LoginValidator validator;
     //private FileOperator fileOperator;
-    private String username;
-    private String password;
+    
+    private User user1;
+    private List<User> listOfTestUsers;
+    private User user2;
 
     @BeforeEach
     public void setUp() {
+        //filename = "test" because this test does not read from file. It makes a mocked list
+        // with user to test LoginValidator.java
         this.validator = new LoginValidator();
+        listOfTestUsers = new ArrayList<>();
+        user1 = new User("", "", "", new ArrayList<>());
+        user2 = new User("testuser", "tester", "Tester TEst", new ArrayList<>());
+        listOfTestUsers.add(user2);
     }
 
     @Test
     @DisplayName("Test if login is legal when username and/or password is empty.")
     public void testIfEmptyFields() {
-        this.username = "";
-        this.password = "gutt123";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.isLoginLegal(username, password));
+        
+        Assertions.assertThrows(IllegalArgumentException.class,
+        () -> validator.isLoginLegal(user1.getUsername(), user1.getPassword(), listOfTestUsers));
 
-        this.username = "Freddy";
-        this.password = "";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.isLoginLegal(username, password));
+        
+        user1.setPassword("Gutt123");
+        Assertions.assertThrows(IllegalArgumentException.class,
+        () -> validator.isLoginLegal(user1.getUsername(), user1.getPassword(), listOfTestUsers));
 
-        this.username = "";
-        this.password = "";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.isLoginLegal(username, password));
+        user1.setPassword("");
+        user1.setUsername("Freddy");
+        Assertions.assertThrows(IllegalArgumentException.class,
+        () -> validator.isLoginLegal(user1.getUsername(), user1.getPassword(), listOfTestUsers));
+
     }
 
     @Test
     @DisplayName("Test if login is legal when password is wrong, and when it exists, but not for this username.")
     public void testIsLoginLegalWhenPasswordIsNotMatching() {
-        this.username = "Freddy";
-        this.password = "ajfofnjqi2";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.isLoginLegal(username, password));
+        
+        user1.setUsername("Freddy");
+        user1.setPassword("ajfofnjqi2");
 
-        this.username = "Freddy";
-        this.password = "werwew";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.isLoginLegal(username, password));
+        Assertions.assertThrows(IllegalArgumentException.class,
+        () -> validator.isLoginLegal(user1.getUsername(), user1.getPassword(), listOfTestUsers));
+
+    
+        user1.setPassword("tester");
+        Assertions.assertThrows(IllegalArgumentException.class,
+        () -> validator.isLoginLegal(user1.getUsername(), user1.getPassword(), listOfTestUsers));
     }
 
     @Test
-    @DisplayName("Test if Login is legal when the username and password are matching and exists in the json file.")
+    @DisplayName("Test if Login is legal when the username and password are matching and exists in the list of user.")
     public void testIsLoginLegalWhenUsernameAndPasswordMatch() {
-        this.username = "Freddy";
-        this.password = "gutt123";
-        Assertions.assertTrue(validator.isLoginLegal(username, password));
-        Assertions.assertDoesNotThrow(() -> validator.isLoginLegal(username, password));
+        Assertions.assertTrue(validator.isLoginLegal(user2.getUsername(), user2.getPassword(), listOfTestUsers));
     }
 
     @Test
     @DisplayName("Test if login is legal when username does not exist")
     public void testIsLoginLegalWhenUserNameDoesNotExist() {
-        this.username = "itpDude";
-        this.password = "heyHEY";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> validator.isLoginLegal(username, password));
+        user1.setUsername("itpDude");
+        user1.setPassword("heyHEY");
+        Assertions.assertThrows(IllegalArgumentException.class,
+         () -> validator.isLoginLegal(user1.getUsername(), user1.getUsername(), listOfTestUsers));
     }
 
 }
