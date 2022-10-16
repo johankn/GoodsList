2c7bc213a4e251026dab999353dc8907042c69aa
Elias Lysø Sommerseth <elias.sommerseth@gamil.com>
2022-10-16 18:25:40 +0200

    Implemented AdSorter.java
    Implemented AdSorter.java. As of for now it only sorts by date.

---
+++ GoodsList/core/src/main/java/core/AdSorter.java
@@ -0,0 +1,55 @@
+package core;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AdSorter {
+
+    /*
+     * This class sorts ads by different preferences.
+     */
+
+    private List<Ad> ads;
+
+    public AdSorter(List<Ad> ads) {
+        this.ads = ads;
+    }
+
+    /**
+     * sorts the list and returs the sortedlist by date.
+     * 
+     * @return List<Ad>
+     */
+    public List<Ad> sortAdsByDate() {
+        List<Ad> sortedAdsByDate = ads;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Collections.sort(sortedAdsByDate, (ad1, ad2) -> LocalDate.parse(ad2.getDate(), formatter)
+                .compareTo(LocalDate.parse(ad1.getDate(), formatter)));
+
+        return sortedAdsByDate;
+
+    }
+
+    /**
+     * @return List<Ad>
+     */
+    public List<Ad> getAds() {
+        return ads;
+
+    }
+
+    /**
+     * @param ads
+     */
+    public void setAds(List<Ad> ads) {
+        this.ads = ads;
+    }
+
+}
