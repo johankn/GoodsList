66777933ec448e2c81b98a9cf2f5b115484df8d9
Elias Lysø Sommerseth <elias.sommerseth@gamil.com>
2022-10-16 14:18:35 +0200

    Added comments in json-folder
    Added comments in core folder for persistence classes

--- GoodsList/core/src/main/java/json/JsonFileAsObject.java
+++ GoodsList/core/src/main/java/json/JsonFileAsObject.java
@@ -8,22 +8,45 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import core.User;
 
 public class JsonFileAsObject {
+
+    /* 
+     * This class represents a jsonfiule with list of users as a java object
+     */
     
     private List<User> users;
 
+    /* 
+     * JsonCreator to make jackson understand that this constructor should be used.
+     * JsonProperty to tell json what to look for in json file when make this object
+     */
     @JsonCreator
     public JsonFileAsObject(@JsonProperty(value = "users") List<User> users) {
         this.users = users;
     }
 
+    
+    /** 
+     * Return the users
+     * @return List<User>
+     */
     public List<User> getUsers() {
         return users;
     }
 
+    
+    /** 
+     * Setter for users
+     * @param users
+     */
     public void setUsers(List<User> users) {
         this.users = users;
     }
 
+    
+    /** 
+     * Adds a user
+     * @param user
+     */
     public void addUser(User user){
         users.add(user);
     }
