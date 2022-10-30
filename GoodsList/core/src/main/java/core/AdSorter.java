3d5f411871f2102060419019d0cf2c09cde0608f
mavalhov <mavalhov@stud.ntnu.no>
2022-10-30 13:40:47 +0100

    Changed all indentations

--- GoodsList/core/src/main/java/core/AdSorter.java
+++ GoodsList/core/src/main/java/core/AdSorter.java
@@ -1,62 +1,61 @@
 package core;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-
 import json.Ad;
 
 public class AdSorter {
 
-    /*
-     * This class sorts ads by different preferences.
-     */
+  /*
+   * This class sorts ads by different preferences.
+   */
 
-    private List<Ad> ads;
+  private List<Ad> ads;
 
-    public AdSorter(List<Ad> ads) {
-        this.ads = ads;
-    }
+  public AdSorter(List<Ad> ads) {
+    this.ads = ads;
+  }
 
-    /**
-     * sorts the list and returs the sortedlist by date.
-     * 
-     * @return List<Ad>
-     */
-    public List<Ad> sortAdsByDate() {
-        List<Ad> sortedAdsByDate = ads;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        Collections.sort(sortedAdsByDate, (ad1, ad2) -> LocalDate.parse(ad2.getDate(), formatter)
+  /**
+   * sorts the list and returs the sortedlist by date.
+   *
+   * @return ads
+   */
+  public List<Ad> sortAdsByDate() {
+    List<Ad> sortedAdsByDate = ads;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    Collections.sort(
+        sortedAdsByDate,
+        (ad1, ad2) ->
+            LocalDate.parse(ad2.getDate(), formatter)
                 .compareTo(LocalDate.parse(ad1.getDate(), formatter)));
 
-        return sortedAdsByDate;
+    return sortedAdsByDate;
+  }
 
-    }
+  public List<String> getAdAttributeInFile(List<Ad> listOfAds, Function<Ad, String> function) {
+    return listOfAds.stream().map(function).toList();
+  }
 
-    public List<String> getAdAttributeInFile(List<Ad> listOfAds, Function<Ad,String> function){
-        return listOfAds.stream().map(function).toList();
-    }
+  /**
+   * returning ads.
+   * 
+   * @return ads
+   */
+  public List<Ad> getAds() {
+    return new ArrayList<Ad>(ads);
+  }
 
-    /**
-     * @return List<Ad>
-     */
-    public List<Ad> getAds() {
-        return new ArrayList<Ad>(ads);
-
-    }
-
-    /**
-     * @param ads
-     */
-    public void setAds(List<Ad> ads) {
-        this.ads = ads;
-    }
-
+  /**
+   * setting ads.
+   * 
+   * @param ads ads
+   */
+  public void setAds(List<Ad> ads) {
+    this.ads = ads;
+  }
 }
--- GoodsList/core/src/main/java/core/RegisteredUser.java
+++ GoodsList/core/src/main/java/core/RegisteredUser.java
@@ -4,58 +4,55 @@ import java.util.ArrayList;
 
 public class RegisteredUser {
 
-    /*
-     * This class represents a registered user in the app.
-     */
+  /*
+   * This class represents a registered user in the app.
+   */
 
-    private String username;
-    private String password;
-    private String fullName;
-    private String repeatedPassword;
+  private String username;
+  private String password;
+  private String fullName;
+  private String repeatedPassword;
 
-    public RegisteredUser(String username, String password, String fullName, String repeatedPassword) {
-        this.username = username;
-        this.password = password;
-        this.fullName = fullName;
-        this.repeatedPassword = repeatedPassword;
-    }
+  public RegisteredUser(
+      String username, String password, String fullName, String repeatedPassword) {
+    this.username = username;
+    this.password = password;
+    this.fullName = fullName;
+    this.repeatedPassword = repeatedPassword;
+  }
 
-    /**
-     * @return String
-     */
-    public String getUsername() {
-        return this.username;
-    }
+  /**
+   * @return String
+   */
+  public String getUsername() {
+    return this.username;
+  }
 
-    /**
-     * @return String
-     */
-    public String getPassword() {
-        return this.password;
-    }
+  /**
+   * @return String
+   */
+  public String getPassword() {
+    return this.password;
+  }
 
-    /**
-     * @return String
-     */
-    public String getFullName() {
-        return this.fullName;
-    }
+  /**
+   * @return String
+   */
+  public String getFullName() {
+    return this.fullName;
+  }
 
-    /**
-     * @return String
-     */
-    public String getRepeatedPassword() {
-        return this.repeatedPassword;
-    }
+  /**
+   * @return String
+   */
+  public String getRepeatedPassword() {
+    return this.repeatedPassword;
+  }
 
-    /**
-     * @return User
-     */
-    public User generateUser() {
-        return new User(this.getUsername(),
-                this.getPassword(),
-                this.getFullName(),
-                new ArrayList<>());
-    }
-
+  /**
+   * @return User
+   */
+  public User generateUser() {
+    return new User(this.getUsername(), this.getPassword(), this.getFullName(), new ArrayList<>());
+  }
 }
