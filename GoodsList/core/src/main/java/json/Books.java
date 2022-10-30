3eae2dd3868e77673419276f3f621f6e4c7a1187
mavalhov <mavalhov@stud.ntnu.no>
2022-10-30 14:04:55 +0100

    Fixed all checkstyle warnings in core/json
    Used google-java-format to auto fix the indentation errors

--- GoodsList/core/src/main/java/json/Books.java
+++ GoodsList/core/src/main/java/json/Books.java
@@ -4,118 +4,108 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
-/**
- * Specifies that this is the books subclass
- * and connects it to the parent class product 
+/** 
+ * Specifies that this is the books subclass and connects it to the parent class product .
  */
-
 @JsonTypeName("book")
 
-/**
- * A child class "Books" that inherits from the parent class "Product".
- * A Books object represents a physical book that can be sold on our site.
- */
 
 public class Books extends Product {
 
-    private int pages;
-    private String author;
-    private String genre;
-    private int releaseYear;
+  private int pages;
+  private String author;
+  private String genre;
+  private int releaseYear;
 
-    /**
-    * A constructor for the class Books
-    * We are using Jackson Annotation to create executing rules for jackson, 
-    * JsonCreator specifies that this is a constructor.
-    * JsonProperty specifies which fields should be set to what in the JSON-file
-    */
+  /**
+   * A constructor for the class Books We are using Jackson Annotation to create executing rules for
+   * jackson, JsonCreator specifies that this is a constructor. JsonProperty specifies which fields
+   * should be set to what in the JSON-file
+   */
+  @JsonCreator
+  public Books(
+      @JsonProperty(value = "price") int price,
+      @JsonProperty(value = "condition") String condition,
+      @JsonProperty(value = "author") String author,
+      @JsonProperty(value = "genre") String genre,
+      @JsonProperty(value = "releaseYear") int releaseYear,
+      @JsonProperty(value = "pages") int pages) {
+    super(price, condition);
+    this.pages = pages;
+    this.author = author;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+  }
 
-    @JsonCreator
-    public Books(
-    @JsonProperty(value = "price") int price,
-    @JsonProperty(value = "condition") String condition,
-    @JsonProperty(value = "author") String author,
-    @JsonProperty(value = "genre") String genre,
-    @JsonProperty(value = "releaseYear") int releaseYear,
-    @JsonProperty(value = "pages") int pages) {
-        super(price, condition);
-        this.pages = pages;
-        this.author = author;
-        this.genre = genre;
-        this.releaseYear = releaseYear;
+  /**
+   * Gets the pages of the book.
+   *
+   * @return int
+   */
+  public int getPages() {
+    return pages;
+  }
 
-    }
+  /**
+   * Sets amount of pages in the book.
+   *
+   * @param pages pages
+   */
+  public void setPages(int pages) {
+    this.pages = pages;
+  }
 
-    
-    /**
-     * Gets the pages of the book
-     * @return int
-     */
-    public int getPages() {
-        return pages;
-    }
+  /**
+   * Gets the author of the book.
+   *
+   * @return String
+   */
+  public String getAuthor() {
+    return author;
+  }
 
-    
-    /**
-     * Sets amount of pages in the book
-     * @param pages
-     */
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
+  /**
+   * Sets the author of the book.
+   *
+   * @param author author
+   */
+  public void setAuthor(String author) {
+    this.author = author;
+  }
 
-    
-    /**
-     * Gets the author of the book
-     * @return String
-     */
-    public String getAuthor() {
-        return author;
-    }
+  /**
+   * Gets the genre of the book.
+   *
+   * @return String
+   */
+  public String getGenre() {
+    return genre;
+  }
 
-    
-    /**
-     * Sets the author of the book
-     * @param author
-     */
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+  /**
+   * Sets the genre of the book.
+   *
+   * @param genre genre
+   */
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
 
-    
-    /**
-     * Gets the genre of the book
-     * @return String
-     */
-    public String getGenre() {
-        return genre;
-    }
+  /**
+   * Gets the release year of the book.
+   *
+   * @return int
+   */
+  public int getReleaseYear() {
+    return releaseYear;
+  }
 
-    
-    /**
-     * Sets the genre of the book
-     * @param genre
-     */
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    
-    /**
-     * Gets the release year of the book
-     * @return int
-     */
-    public int getReleaseYear() {
-        return releaseYear;
-    }
-
-    
-    /**
-     * Sets the release year of the book
-     * @param releaseYear
-     */
-    public void setReleaseYear(int releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
+  /**
+   * Sets the release year of the book.
+   *
+   * @param releaseYear year
+   */
+  public void setReleaseYear(int releaseYear) {
+    this.releaseYear = releaseYear;
+  }
 }
--- GoodsList/core/src/main/java/json/UserInfoCollector.java
+++ GoodsList/core/src/main/java/json/UserInfoCollector.java
@@ -1,43 +1,45 @@
 package json;
 
+import core.User;
 import java.util.List;
 
-import core.User;
+/**
+ * Class for getting info about a user.
+ */
 public class UserInfoCollector {
-    /* 
-     *This class find info about the user by information we allready know about it.
-    */
-    
+  /**
+   *This class find info about the user by information we allready know about it.
+   */
 
-    private FileOperator fileOperator;
+  private FileOperator fileOperator;
 
-    public UserInfoCollector() {
-        this.fileOperator = new FileOperator();
-    }
-    
-    /** 
-     * Gets the full name of a user by knowing its user name.
-     * @param filename
-     * @param userName
-     * @return String
-     */
-    //Finds the full name of a user by its username.
-    public String getFullNameByUsername(String filename, String userName){
-        List<User> allExistingUsers = fileOperator.getAllUsersAsList(filename);
-        for (int i = 0; i < allExistingUsers.size(); i++) {
-            if(userName.equals(allExistingUsers.get(i).getUsername())){
-                return allExistingUsers.get(i).getFullname();
-            }
-        }
-        throw new NullPointerException("This user does not exist");
-    }
+  public UserInfoCollector() {
+    this.fileOperator = new FileOperator();
+  }
 
-    
-    /** 
-     * getter for fileoerator-field-
-     * @return FileOperator
-     */
-    public FileOperator getFileOperator() {
-        return fileOperator;
+  /**
+   * Gets the full name of a user by knowing its user name.
+   *
+   * @param filename filename
+   * @param userName username
+   * @return String
+   */
+  public String getFullNameByUsername(String filename, String userName) {
+    List<User> allExistingUsers = fileOperator.getAllUsersAsList(filename);
+    for (int i = 0; i < allExistingUsers.size(); i++) {
+      if (userName.equals(allExistingUsers.get(i).getUsername())) {
+        return allExistingUsers.get(i).getFullname();
+      }
     }
+    throw new NullPointerException("This user does not exist");
+  }
+
+  /**
+   * getter for fileoerator-field.
+   *
+   * @return FileOperator
+   */
+  public FileOperator getFileOperator() {
+    return fileOperator;
+  }
 }
